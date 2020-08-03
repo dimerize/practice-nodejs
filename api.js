4b4c0b1780dc3ae47b30bdf4982aaa58ebdbaa8d
Omar Khan <ok2282@columbia.edu>
2020-08-03 13:14:29 -0500

    nodemon

--- api.js
+++ api.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
 const app = express();
-const bookRouter = express.Router();
+// const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 
-bookRouter.route('/books')
+/* bookRouter.route('/books')
     .get((req, res) => {
         const response = { hello: 'API message'};
         res.json(response);
     });
+*/
+
+app.get('/', (req, res) => {
+    res.send('hi chicken');
+});
+
+app.listen(port, () => {
+    console.log("hi"+port);
+})
