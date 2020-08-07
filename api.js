d033395f2abb442da00a90d1e0ec18b2b33010f9
Omar Khan <ok2282@columbia.edu>
2020-08-07 01:42:04 -0500

    Implemented HTTP GET

--- api.js
+++ api.js
@@ -5,11 +5,31 @@ const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
+const e = require('express');
 
 bookRouter.route('/books')
     .get((req, res) => {
-        const response = { hello: ' message'};
-        res.json(response);
+        const query = {};
+        if(req.query.genre) {
+            query.genre = req.query.genre;
+        }
+        Book.find(query, (err, books) => {
+            if(err) {
+                return res.send(err);
+            }
+            return res.json(books);
+        });
+    });
+
+bookRouter.route('/books/:bookId')
+    .get((req, res) => {
+        Book.findById(req.params.bookId, (err, book) => {
+            if(err) {
+                return res.send(err);
+            }
+            return res.json(book);
+        });
     });
 
 app.use('/api', bookRouter);
